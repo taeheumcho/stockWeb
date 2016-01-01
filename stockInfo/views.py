170d6a9763a28d9240f3aa4a07e583be42483c69
thCho <taeheumcho@gmail.com>
2016-01-02 01:19:03 +0900

    f

--- stockInfo/views.py
+++ stockInfo/views.py
@@ -1,46 +1,16 @@
 # from highcharts.views import HighChartsLineView
 # import json, highcharts
-import code
-import json
-from mimetypes import MimeTypes
-import pdb
-from time import timezone
 
-from _mysql import NULL
-from chartit import Chart, DataPool
-from django.core import serializers
+import json
 from django.http.response import HttpResponseRedirect, JsonResponse, \
     HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.template.context_processors import request
-from django.views.generic.base import TemplateView
-from lxml.html.builder import CODE
-
-from .forms import stockInfoForm
-from .models import StockInfo, StockSisae, ZzImsiSisae
-from json.encoder import JSONEncoder
-from stockInfo.models import CcImsiData, ImsiIndexData
-from django.utils.safestring import mark_safe
 from django.template.context import RequestContext
-import operator
+from django.template.context_processors import request
 
-# Create your views here.
-data = []
-def stockHome(request):
-#     if request.method == "POST":  
-#         entity = StockInfo.objects.get(pk = request.POST['uri'])
-# #         pdb.set_trace()           
-# #         form = stockInfoForm(request.POST, instance = entity)
-# #         if form.is_valid():
-# #         info = form.save(commit = False)
-# #         info.save()
-# 
-#         return redirect('stockInfo.views.stock_detail2', code = entity.code)
-# #         else :
-# #             render(request, 'stockInfo/InfoDirector.html')
-#     else :
-#         form = stockInfoForm()
-#         
+from stockInfo.models import CcImsiData, ImsiIndexData
+
+def stockHome(request):       
     tmpFX = CcImsiData.objects.filter(code = 'FX_USDKRW').values('date','value').order_by('date')
     closestdate_FX = tmpFX[len(tmpFX) - 1]['value']
     
@@ -53,6 +23,9 @@ def stockHome(request):
     result_list = json.dumps(list(tmpKOSDAQ))
     response = JsonResponse(result_list, safe=False)
     
-    return render(request, 'stockInfo/newHome.html',{'FX': closestdate_FX, 'KOSPI': closestdate_KOSPI, 'KOSDAQ': closestdate_KOSDAQ, 'ChartData': result_list})
+    return render(request, 'stockInfo/home.html',{'FX': closestdate_FX, 'KOSPI': closestdate_KOSPI, 'KOSDAQ': closestdate_KOSDAQ, 'ChartData': result_list})
 
 
+def backTestHome(request):
+    
+    return render(request, 'stockInfo/backTestHome.html')
--- stockWeb/settings.py
+++ stockWeb/settings.py
@@ -78,7 +78,12 @@ WSGI_APPLICATION = 'stockWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        '''''''
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kirudadb',
+        'USER': 'admin',
+        'PASSWORD':'1qaz2wsx',
+        'HOST':'kirudadb.c5rq93advanj.us-west-2.rds.amazonaws.com',
+        'PORT':'3306',
     }
 }
 
